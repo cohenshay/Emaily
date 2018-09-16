955b36ba35fa4d82bda96ff15a040416ab05791e
cohen shay <cohenshay85@gmail.com>
2018-09-16 16:59:22 +0300

    cannot upload to heroku

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
 require("./models/User");
-require("./services/passport")
-
+require("./services/passport");
 
 mongoose.connect(keys.mongoDbURI);
 
@@ -19,15 +18,27 @@ app.use(
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //express will serve up proction assets
+  //like our main.js file
 
+  app.use(express.static("client/build"));
 
+  //express will serve up the index.html file
+  //if it doesn't recognize the route
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
